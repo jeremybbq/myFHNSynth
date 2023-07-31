31dce974856f255bc2de2ae1e39838239f8da2ee
jeremybbq <jeremy.bbq@gmail.com>
2023-07-31 16:25:09 +0100

    Use generic editor

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -202,7 +202,7 @@ bool MyFHNSynthAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* MyFHNSynthAudioProcessor::createEditor()
 {
-    return new MyFHNSynthAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor (*this);
 }
 
 //==============================================================================
@@ -211,12 +211,23 @@ void MyFHNSynthAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    auto state = parameterTree.copyState();
+    std::unique_ptr<juce::XmlElement> xml(state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 void MyFHNSynthAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+    if (xmlState.get() != nullptr)
+    {
+        if (xmlState->hasTagName(parameterTree.state.getType()))
+        {
+            parameterTree.replaceState(juce::ValueTree::fromXml(*xmlState));
+        }
+    }
 }
 
 //==============================================================================
